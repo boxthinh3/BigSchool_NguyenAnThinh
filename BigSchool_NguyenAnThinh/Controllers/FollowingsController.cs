ec5ae128a1a87d8747019564bd2aa7d2c1a6dac9
boxthinh3 <127471847+boxthinh3@users.noreply.github.com>
2023-03-27 23:12:45 +0700

    Add project files.

---
+++ BigSchool_NguyenAnThinh/Controllers/FollowingsController.cs
@@ -0,0 +1,42 @@
+﻿using BigSchool_NguyenAnThinh.DTOs;
+using BigSchool_NguyenAnThinh.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace BigSchool_NguyenAnThinh.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+                return BadRequest("Following already exists!");
+
+            var folowing = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(folowing);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ BigSchool_NguyenAnThinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_NguyenAnThinh.Startup))]
+namespace BigSchool_NguyenAnThinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
